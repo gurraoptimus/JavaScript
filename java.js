5b16d5350d88ebebee1694f84fe2f2012dab9a3f
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2021-08-28 15:42:24 +0200

    T3st
    console.log java script
    node Java.js

---
+++ java.js
@@ -0,0 +1,20 @@
+var age=21;
+var planet="Earth";
+if (age>=21)
+
+{
+
+console.log("you are an adult");
+
+console.log("you are over 21");
+}
+
+else
+
+{
+
+console.log("you are not an adult");
+
+console.log("you are not over 18");
+}
+

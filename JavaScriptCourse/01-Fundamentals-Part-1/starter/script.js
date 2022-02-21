7cf1b403a215688d05fed50819565c24eea79f53
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2022-02-21 15:14:00 +0100

    console.log

--- JavaScriptCourse/01-Fundamentals-Part-1/starter/script.js
+++ JavaScriptCourse/01-Fundamentals-Part-1/starter/script.js
@@ -3,3 +3,9 @@ if(js === "js") alert("js is cool");
      
  40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("jonas");
+console.log(23);
+
+let firstName = "Belle Delphine";
+console.log(firstName);

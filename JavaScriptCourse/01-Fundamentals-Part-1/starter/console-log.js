c3ffccf8514994447251a71ae2e16e85ab48f2b6
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2022-02-21 18:02:22 +0100

    console.log

---
+++ JavaScriptCourse/01-Fundamentals-Part-1/starter/console-log.js
@@ -0,0 +1,24 @@
+let country = "Sverige";
+let continent = "Europa";
+let population = 1149381;
+
+console.log("Jag bor i " + country + ".");
+
+console.log(country + " ligger i " + continent + ".");
+
+//console.log("Det bor " + population +" människor i " + country +".");
+
+
+// ÖVERKURS 
+
+var isElevenMillions = false;
+
+if(population > 1100000){
+    isElevenMillions = true;
+}
+
+if(!isElevenMillions){
+    console.log("Det bor " + population +" människor i " + country +".");
+} else if(isElevenMillions){
+    console.log("Det bor mer än 10 miljoner i " + country);
+}
